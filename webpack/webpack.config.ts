Vessel9817/source-inspector

commit d97837c3a15063873da730ff0516b81f23af5069

Fixed file paths in bundled files
Side effect: building in production mode now incurs an infinite loop- which is still usable, but annoying

Signed-off-by: Anonymous-Humanoid <68610963+Anonymous-Humanoid@users.noreply.github.com>

webpack/webpack.config.ts
@@ -8,7 +8,8 @@ import webpack, {
     Compiler,
     Configuration,
     ProgressPlugin,
-    WebpackPluginInstance
+    WebpackPluginInstance,
+    AssetInfo
 } from 'webpack';
 import {
     Source as _Source,
@@ -20,15 +21,19 @@ import {
 // Non-secret env vars are defined in nodemon config
 const NODE_ENV = process.env.NODE_ENV;
 const PROJECT_ROOT = path.join(__dirname, '..');
-const OUTPUT_REL_DIR = process.env.OUTPUT_DIR!.replaceAll('\\', '/'); // Safety
+const OUTPUT_DIR_NAME = process.env.OUTPUT_DIR_NAME!;
 const CHROME_DIR_NAME = 'chrome';
 const FIREFOX_DIR_NAME = 'firefox';
 
-const OUTPUT_ABS_DIR = path.join(PROJECT_ROOT, OUTPUT_REL_DIR);
-const CHROME_ABS_DIR = path.join(PROJECT_ROOT, OUTPUT_REL_DIR, CHROME_DIR_NAME);
+const OUTPUT_ABS_DIR = path.join(PROJECT_ROOT, OUTPUT_DIR_NAME);
+const CHROME_ABS_DIR = path.join(
+    PROJECT_ROOT,
+    OUTPUT_DIR_NAME,
+    CHROME_DIR_NAME
+);
 const FIREFOX_ABS_DIR = path.join(
     PROJECT_ROOT,
-    OUTPUT_REL_DIR,
+    OUTPUT_DIR_NAME,
     FIREFOX_DIR_NAME
 );
 
@@ -56,22 +61,26 @@ class GenerateFilePlugin implements WebpackPluginInstance {
     private readonly plugin: Tap = { name: 'GenerateFilePlugin' };
     private readonly filepath: string;
     private readonly source: Source;
+    private readonly assetInfo: AssetInfo | undefined;
 
-    constructor(filename: string, content: string) {
+    constructor(filename: string, content: string, assetInfo?: AssetInfo) {
         this.filepath = filename;
         this.source = new Source();
         this.source.source = () => content;
+        this.assetInfo = assetInfo;
     }
 
     public apply(compiler: Compiler) {
         compiler.hooks.compilation.tap(this.plugin, (compilation) => {
             compilation.emitAsset(this.filepath, this.source, {
+                ...this.assetInfo,
                 sourceFilename: path.basename(this.filepath)
             });
         });
         this.source.updateHash = (hash: HashLike) => {
             compiler.hooks.compilation.tap(this.plugin, (compilation) => {
                 compilation.updateAsset(this.filepath, this.source, {
+                    ...this.assetInfo,
                     contenthash: hash.digest().toString(),
                     sourceFilename: path.basename(this.filepath)
                 });
@@ -87,11 +96,13 @@ class GenerateFilePlugin implements WebpackPluginInstance {
      */
     public static generateManifestPlugin(
         filename: string,
-        manifest: chrome.runtime.Manifest
+        manifest: chrome.runtime.Manifest,
+        assetInfo?: AssetInfo
     ): GenerateFilePlugin {
         return new GenerateFilePlugin(
             filename,
-            GenerateFilePlugin.stringifyManifest(manifest)
+            GenerateFilePlugin.stringifyManifest(manifest),
+            assetInfo
         );
     }
 
@@ -189,7 +200,7 @@ const SHARED_PLUGINS: Configuration['plugins'] = [
                         'icons',
                         inputPath
                     ),
-                    to: path.join(CHROME_ABS_DIR, outputPath)
+                    to: path.join(OUTPUT_ABS_DIR, outputPath)
                 }
             ]
         });
@@ -204,7 +215,7 @@ const SHARED_PLUGINS: Configuration['plugins'] = [
             'popup',
             'index.html'
         ),
-        filename: path.join(CHROME_ABS_DIR, 'popup', 'index.html'),
+        filename: path.join('popup', 'index.html'),
         chunks: ['popup']
     })
 ];
@@ -213,25 +224,50 @@ const BROWSER_PLUGINS: Configuration['plugins'] = [
     new CopyWebpackPlugin({
         patterns: [
             {
-                from: CHROME_ABS_DIR,
-                to: FIREFOX_ABS_DIR,
+                from: OUTPUT_ABS_DIR,
+                to: CHROME_ABS_DIR,
                 noErrorOnMissing: true,
-                force: false,
                 globOptions: {
-                    ignore: ['**/manifest.json']
+                    ignore: [`**/${CHROME_DIR_NAME}`, `**/${FIREFOX_DIR_NAME}`]
                 }
             }
         ]
     }),
+    new CopyWebpackPlugin({
+        patterns: [
+            {
+                from: CHROME_ABS_DIR,
+                to: FIREFOX_ABS_DIR,
+                noErrorOnMissing: true,
+                globOptions: {
+                    ignore: [`**/${FIREFOX_DIR_NAME}`, '**/manifest.json']
+                }
+            }
+        ]
+    }),
+    // new FilemanagerWebpackPlugin({
+    //     events: {
+    //         onEnd: {
+    //             delete: [
+    //                 {
+    //                     source: path.join(OUTPUT_ABS_DIR, 'popup'),
+    //                     options: {}
+    //                 }
+    //             ]
+    //         }
+    //     }
+    // }),
 
     // Generating manifest files
     GenerateFilePlugin.generateManifestPlugin(
         path.join(CHROME_DIR_NAME, 'manifest.json'),
-        CHROME_MANIFEST
+        CHROME_MANIFEST,
+        { minimized: !IS_DEV_MODE }
     )
     // new GenerateFilePlugin.generateManifestPlugin(
     //     path.join(FIREFOX_DIR_NAME, 'manifest.json'),
-    //     FIREFOX_MANIFEST
+    //     FIREFOX_MANIFEST,
+    //     { minimized: !IS_DEV_MODE }
     // )
 ];
 
@@ -260,16 +296,14 @@ const config: webpack.Configuration = {
                     'docListener.ts'
                 )
             ],
-            filename: path.join(CHROME_DIR_NAME, 'popup', 'docListener.js')
+            filename: path.join('popup', 'docListener.js')
         },
         popup: {
             import: [
                 path.join(PROJECT_ROOT, 'src', 'pages', 'popup', 'index.tsx')
             ],
             // HTMLWebpackPlugin requires forward slashes
-            filename: path
-                .join(CHROME_DIR_NAME, 'popup', 'index.js')
-                .replaceAll('\\', '/')
+            filename: path.join('popup', 'index.js').replaceAll('\\', '/')
         },
 
         // Background
@@ -283,7 +317,7 @@ const config: webpack.Configuration = {
                     'index.ts'
                 )
             ],
-            filename: path.join(CHROME_DIR_NAME, 'background', 'index.js')
+            filename: path.join('background', 'index.js')
         }
     },
     output: {

webpack/webserver.ts
@@ -4,7 +4,7 @@ import WebpackDevServer from 'webpack-dev-server';
 import config from './webpack.config';
 
 const compiler = webpack(config);
-const OUTPUT_DIR = process.env.OUTPUT_DIR!;
+const OUTPUT_DIR_NAME = process.env.OUTPUT_DIR_NAME!;
 const WEBPACK_PORT = process.env.WEBPACK_PORT!;
 
 // Enabling server-side (and disabling client-side) hot reloading
@@ -16,7 +16,7 @@ const server = new WebpackDevServer(
         webSocketServer: false,
         bonjour: false,
         static: {
-            directory: resolve(process.cwd(), OUTPUT_DIR)
+            directory: resolve(process.cwd(), OUTPUT_DIR_NAME)
         },
         devMiddleware: {
             writeToDisk: true

commit c0cfd306d37f0ea26c4edcdfa8394022170b0bf6

Split Chrome and Firefox builds into separate Nodemon configurations
Consequently, this patches the infinite loop caused by d97837c3a15063873da730ff0516b81f23af5069

Signed-off-by: Anonymous-Humanoid <68610963+Anonymous-Humanoid@users.noreply.github.com>

webpack/webpack.config.ts
@@ -5,11 +5,10 @@ import path from 'path';
 import { Tap } from 'tapable';
 import TerserPlugin from 'terser-webpack-plugin';
 import webpack, {
+    AssetInfo,
     Compiler,
-    Configuration,
     ProgressPlugin,
-    WebpackPluginInstance,
-    AssetInfo
+    WebpackPluginInstance
 } from 'webpack';
 import {
     Source as _Source,
@@ -21,26 +20,16 @@ import {
 // Non-secret env vars are defined in nodemon config
 const NODE_ENV = process.env.NODE_ENV;
 const PROJECT_ROOT = path.join(__dirname, '..');
-const OUTPUT_DIR_NAME = process.env.OUTPUT_DIR_NAME!;
-const CHROME_DIR_NAME = 'chrome';
-const FIREFOX_DIR_NAME = 'firefox';
-
-const OUTPUT_ABS_DIR = path.join(PROJECT_ROOT, OUTPUT_DIR_NAME);
-const CHROME_ABS_DIR = path.join(
-    PROJECT_ROOT,
-    OUTPUT_DIR_NAME,
-    CHROME_DIR_NAME
-);
-const FIREFOX_ABS_DIR = path.join(
-    PROJECT_ROOT,
-    OUTPUT_DIR_NAME,
-    FIREFOX_DIR_NAME
-);
+const BROWSER = process.env.BROWSER!;
+const OUTPUT_DIR = path.join(process.env.OUTPUT_DIR!, BROWSER);
+const OUTPUT_ABS_DIR = path.join(PROJECT_ROOT, OUTPUT_DIR);
 
 // Verifying node env
 if (NODE_ENV == null) {
     throw new Error('Node environment must be specified');
 }
+
+const IS_DEV_MODE = NODE_ENV !== 'production';
 
 type SourceAndMap = _SourceAndMap & { map: Object };
 
@@ -121,8 +110,6 @@ class GenerateFilePlugin implements WebpackPluginInstance {
     }
 }
 
-const IS_DEV_MODE = process.env.NODE_ENV !== 'production';
-
 // Copying icons
 const VALID_SIZES = [16, 32, 48, 128].map((size) => size.toString());
 const RELATIVE_ICON_PATHS = [
@@ -165,6 +152,10 @@ const CHROME_MANIFEST: chrome.runtime.ManifestV3 = {
     icons: MANIFEST_ICON_PATHS
 };
 // const FIREFOX_MANIFEST: chrome.runtime.ManifestV2 = {};
+const MANIFESTS = new Map<string, chrome.runtime.Manifest>([
+    ['chrome', CHROME_MANIFEST]
+    // ['firefox', FIREFOX_MANIFEST]
+]);
 
 // Initializing webpack config
 const STATIC_FILE_EXTS = [
@@ -178,97 +169,6 @@ const STATIC_FILE_EXTS = [
     'ttf',
     'woff',
     'woff2'
-];
-
-const SHARED_PLUGINS: Configuration['plugins'] = [
-    // Setting up fresh Webpack environment
-    new CleanWebpackPlugin({
-        verbose: false,
-        protectWebpackAssets: false
-    }),
-    new ProgressPlugin(),
-
-    // Packaging icons
-    ...RELATIVE_ICON_PATHS.map(([size, [inputPath, outputPath]]) => {
-        return new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.join(
-                        PROJECT_ROOT,
-                        'src',
-                        'assets',
-                        'icons',
-                        inputPath
-                    ),
-                    to: path.join(OUTPUT_ABS_DIR, outputPath)
-                }
-            ]
-        });
-    }),
-
-    // Packaging popup entry point
-    new HtmlWebpackPlugin({
-        template: path.join(
-            PROJECT_ROOT,
-            'src',
-            'pages',
-            'popup',
-            'index.html'
-        ),
-        filename: path.join('popup', 'index.html'),
-        chunks: ['popup']
-    })
-];
-const BROWSER_PLUGINS: Configuration['plugins'] = [
-    // Copying shared files to each browser-specific build directory
-    new CopyWebpackPlugin({
-        patterns: [
-            {
-                from: OUTPUT_ABS_DIR,
-                to: CHROME_ABS_DIR,
-                noErrorOnMissing: true,
-                globOptions: {
-                    ignore: [`**/${CHROME_DIR_NAME}`, `**/${FIREFOX_DIR_NAME}`]
-                }
-            }
-        ]
-    }),
-    new CopyWebpackPlugin({
-        patterns: [
-            {
-                from: CHROME_ABS_DIR,
-                to: FIREFOX_ABS_DIR,
-                noErrorOnMissing: true,
-                globOptions: {
-                    ignore: [`**/${FIREFOX_DIR_NAME}`, '**/manifest.json']
-                }
-            }
-        ]
-    }),
-    // new FilemanagerWebpackPlugin({
-    //     events: {
-    //         onEnd: {
-    //             delete: [
-    //                 {
-    //                     source: path.join(OUTPUT_ABS_DIR, 'popup'),
-    //                     options: {}
-    //                 }
-    //             ]
-    //         }
-    //     }
-    // }),
-
-    // Generating manifest files
-    GenerateFilePlugin.generateManifestPlugin(
-        path.join(CHROME_DIR_NAME, 'manifest.json'),
-        CHROME_MANIFEST,
-        { minimized: !IS_DEV_MODE }
-    )
-    // new GenerateFilePlugin.generateManifestPlugin(
-    //     path.join(FIREFOX_DIR_NAME, 'manifest.json'),
-    //     FIREFOX_MANIFEST,
-    //     { minimized: !IS_DEV_MODE }
-    // )
 ];
 
 const config: webpack.Configuration = {
@@ -399,7 +299,53 @@ const config: webpack.Configuration = {
             }
         ]
     },
-    plugins: [...SHARED_PLUGINS, ...BROWSER_PLUGINS].filter(Boolean),
+    plugins: [
+        // Setting up fresh Webpack environment
+        new CleanWebpackPlugin({
+            verbose: false,
+            protectWebpackAssets: false
+        }),
+        new ProgressPlugin(),
+
+        // Packaging icons
+        ...RELATIVE_ICON_PATHS.map(([size, [inputPath, outputPath]]) => {
+            return new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.join(
+                            PROJECT_ROOT,
+                            'src',
+                            'assets',
+                            'icons',
+                            inputPath
+                        ),
+                        to: path.join(OUTPUT_ABS_DIR, outputPath)
+                    }
+                ]
+            });
+        }),
+
+        // Packaging popup entry point
+        new HtmlWebpackPlugin({
+            template: path.join(
+                PROJECT_ROOT,
+                'src',
+                'pages',
+                'popup',
+                'index.html'
+            ),
+            filename: path.join('popup', 'index.html'),
+            chunks: ['popup']
+        }),
+
+        // Generating manifest files
+        MANIFESTS.has(BROWSER) &&
+            GenerateFilePlugin.generateManifestPlugin(
+                'manifest.json',
+                MANIFESTS.get(BROWSER)!,
+                { minimized: !IS_DEV_MODE }
+            )
+    ].filter(Boolean),
     infrastructureLogging: {
         level: 'info'
     }

webpack/webserver.ts
@@ -4,7 +4,7 @@ import WebpackDevServer from 'webpack-dev-server';
 import config from './webpack.config';
 
 const compiler = webpack(config);
-const OUTPUT_DIR_NAME = process.env.OUTPUT_DIR_NAME!;
+const OUTPUT_DIR = process.env.OUTPUT_DIR!;
 const WEBPACK_PORT = process.env.WEBPACK_PORT!;
 
 // Enabling server-side (and disabling client-side) hot reloading
@@ -16,7 +16,7 @@ const server = new WebpackDevServer(
         webSocketServer: false,
         bonjour: false,
         static: {
-            directory: resolve(process.cwd(), OUTPUT_DIR_NAME)
+            directory: resolve(process.cwd(), OUTPUT_DIR)
         },
         devMiddleware: {
             writeToDisk: true