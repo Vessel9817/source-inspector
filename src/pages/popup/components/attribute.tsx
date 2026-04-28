Vessel9817/source-inspector

commit 4aab0a64b6290e6a8d91d025d8fa1e61f9b9abc0

Implemented message validation and clarified README
Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

src/pages/popup/components/attribute.tsx
@@ -1,6 +1,7 @@
+import assert from 'node:assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
-import { BaseUpdateMsg } from '../msgs';
+import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';
 
 type SharedValues = {
     id: string;
@@ -12,6 +13,15 @@ type SharedValues = {
 };
 
 export type UpdateAttributeMsg = BaseUpdateMsg & SharedValues;
+
+export function validateUpdateAttributeMsg(
+    msg: Readonly<unknown>
+): asserts msg is UpdateAttributeMsg {
+    validateBaseUpdateMsg(msg);
+    assert.ok(msg.nodeType === Node.ATTRIBUTE_NODE);
+    assert.ok(msg.parentId != null);
+    assert.ok(msg.prevSiblingId === undefined);
+}
 
 export type StoredVirtualAttributeProps = StoredVirtualNodeProps &
     SharedValues &

src/pages/popup/components/cdataSection.tsx
@@ -1,6 +1,7 @@
+import assert from 'node:assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
-import { BaseUpdateMsg } from '../msgs';
+import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';
 import { VirtualInlineText } from './text';
 
 interface SharedValues {
@@ -12,6 +13,16 @@ interface SharedValues {
 }
 
 export type UpdateCdataSectionMsg = BaseUpdateMsg & SharedValues;
+
+export function validateUpdateCdataSectionMsg(
+    msg: Readonly<unknown>
+): asserts msg is UpdateCdataSectionMsg {
+    validateBaseUpdateMsg(msg);
+    assert.ok(msg.nodeType === Node.CDATA_SECTION_NODE);
+    assert.ok(msg.nodeName === '#cdata-section');
+    assert.ok(msg.nodeValue != null);
+    assert.ok(msg.parentId != null);
+}
 
 export type StoredVirtualCdataSectionProps = StoredVirtualNodeProps &
     SharedValues &

src/pages/popup/components/comment.tsx
@@ -1,6 +1,7 @@
+import assert from 'node:assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
-import { BaseUpdateMsg } from '../msgs';
+import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';
 
 interface SharedValues {
     parentId: string;
@@ -11,6 +12,16 @@ interface SharedValues {
 }
 
 export type UpdateCommentMsg = BaseUpdateMsg & SharedValues;
+
+export function validateUpdateCommentMsg(
+    msg: Readonly<unknown>
+): asserts msg is UpdateCommentMsg {
+    validateBaseUpdateMsg(msg);
+    assert.ok(msg.nodeType === Node.COMMENT_NODE);
+    assert.ok(msg.nodeName === '#comment');
+    assert.ok(msg.nodeValue != null);
+    assert.ok(msg.parentId != null);
+}
 
 export type StoredVirtualCommentProps = StoredVirtualNodeProps &
     SharedValues &

src/pages/popup/components/doctype.tsx
@@ -1,6 +1,7 @@
+import assert from 'node:assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
-import { BaseUpdateMsg } from '../msgs';
+import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';
 
 interface SharedValues {
     parentId: string;
@@ -12,6 +13,19 @@ interface SharedValues {
 }
 
 export type UpdateDoctypeMsg = BaseUpdateMsg & SharedValues;
+
+export function validateUpdateDoctypeMsg(
+    msg: Readonly<unknown>
+): asserts msg is UpdateDoctypeMsg {
+    validateBaseUpdateMsg(msg);
+    assert.ok(msg.nodeType === Node.ATTRIBUTE_NODE);
+    assert.ok(msg.nodeValue === null);
+    assert.ok(msg.parentId != null);
+    assert.ok('publicId' in msg);
+    assert.ok(typeof msg.publicId === 'string');
+    assert.ok('systemId' in msg);
+    assert.ok(typeof msg.systemId === 'string');
+}
 
 export type StoredVirtualDoctypeProps = StoredVirtualNodeProps &
     SharedValues &

src/pages/popup/components/document.tsx
@@ -1,6 +1,7 @@
+import assert from 'node:assert';
 import React, { ReactNode } from 'react';
 import { NonStoredProps, StoredVirtualNodeProps } from '../base';
-import { BaseUpdateMsg } from '../msgs';
+import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';
 
 interface SharedValues {
     nodeType: Node['DOCUMENT_NODE'];
@@ -11,6 +12,18 @@ interface SharedValues {
 }
 
 export type UpdateDocumentMsg = BaseUpdateMsg & SharedValues;
+
+export function validateUpdateDocumentMsg(
+    msg: Readonly<unknown>
+): asserts msg is UpdateDocumentMsg {
+    validateBaseUpdateMsg(msg);
+    assert.ok(msg.nodeType === Node.DOCUMENT_NODE);
+    assert.ok(msg.nodeName === '#document');
+    assert.ok(msg.nodeValue === null);
+    assert.ok(msg.prevSiblingId === undefined);
+    assert.ok('documentURI' in msg);
+    assert.ok(typeof msg.documentURI === 'string');
+}
 
 export type StoredVirtualDocumentProps = StoredVirtualNodeProps & SharedValues;
 

src/pages/popup/components/element.tsx
@@ -1,6 +1,7 @@
+import assert from 'node:assert';
 import React, { ReactNode } from 'react';
 import { NonStoredProps, StoredVirtualNodeProps } from '../base';
-import { BaseUpdateMsg } from '../msgs';
+import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';
 
 interface SharedValues {
     nodeType: Node['ELEMENT_NODE'];
@@ -12,6 +13,13 @@ export type StoredVirtualElementProps = StoredVirtualNodeProps &
     SharedValues & { attributeIds: Set<string> };
 
 export type VirtualElementProps = NonStoredProps<StoredVirtualElementProps>;
+
+export function validateUpdateElementMsg(
+    msg: Readonly<unknown>
+): asserts msg is UpdateElementMsg {
+    validateBaseUpdateMsg(msg);
+    assert.ok(msg.nodeType === Node.ELEMENT_NODE);
+}
 
 export function VirtualElement(
     props: Readonly<VirtualElementProps>

src/pages/popup/components/processingInstruction.tsx
@@ -1,7 +1,8 @@
+import assert from 'node:assert';
 import React, { ReactNode } from 'react';
-import { BaseUpdateMsg } from '../msgs';
-import { VirtualInlineText } from './text';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
+import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';
+import { VirtualInlineText } from './text';
 
 export interface SharedValues {
     parentId: string;
@@ -11,6 +12,15 @@ export interface SharedValues {
 }
 
 export type UpdateProcessingInstructionMsg = BaseUpdateMsg & SharedValues;
+
+export function validateUpdateProcessingInstructionMsg(
+    msg: Readonly<unknown>
+): asserts msg is UpdateProcessingInstructionMsg {
+    validateBaseUpdateMsg(msg);
+    assert.ok(msg.nodeType === Node.PROCESSING_INSTRUCTION_NODE);
+    assert.ok(msg.nodeValue != null);
+    assert.ok(msg.parentId != null);
+}
 
 export type StoredVirtualProcessingInstructionProps = StoredVirtualNodeProps &
     SharedValues &

src/pages/popup/components/text.tsx
@@ -1,6 +1,7 @@
+import assert from 'node:assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
-import { BaseUpdateMsg } from '../msgs';
+import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';
 
 interface SharedValues {
     parentId: string;
@@ -11,6 +12,15 @@ interface SharedValues {
 }
 
 export type UpdateTextMsg = BaseUpdateMsg & SharedValues;
+
+export function validateUpdateTextMsg(
+    msg: Readonly<unknown>
+): asserts msg is UpdateTextMsg {
+    validateBaseUpdateMsg(msg);
+    assert.ok(msg.nodeType === Node.TEXT_NODE);
+    assert.ok(msg.nodeName === '#text');
+    assert.ok(msg.nodeValue != null);
+}
 
 export type StoredVirtualTextProps = StoredVirtualNodeProps &
     SharedValues &

commit 53fb6c3150d8ac12f0dbc924be42410ee4cc3815

Polyfilled `node:assert`
Fixes a bug introduced in 4aab0a64b6290e6a8d91d025d8fa1e61f9b9abc0

Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

src/pages/popup/components/attribute.tsx
@@ -1,4 +1,4 @@
-import assert from 'node:assert';
+import assert from 'assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
 import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';

src/pages/popup/components/cdataSection.tsx
@@ -1,4 +1,4 @@
-import assert from 'node:assert';
+import assert from 'assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
 import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';

src/pages/popup/components/comment.tsx
@@ -1,4 +1,4 @@
-import assert from 'node:assert';
+import assert from 'assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
 import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';

src/pages/popup/components/doctype.tsx
@@ -1,4 +1,4 @@
-import assert from 'node:assert';
+import assert from 'assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
 import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';

src/pages/popup/components/document.tsx
@@ -1,4 +1,4 @@
-import assert from 'node:assert';
+import assert from 'assert';
 import React, { ReactNode } from 'react';
 import { NonStoredProps, StoredVirtualNodeProps } from '../base';
 import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';

src/pages/popup/components/element.tsx
@@ -1,4 +1,4 @@
-import assert from 'node:assert';
+import assert from 'assert';
 import React, { ReactNode } from 'react';
 import { NonStoredProps, StoredVirtualNodeProps } from '../base';
 import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';

src/pages/popup/components/processingInstruction.tsx
@@ -1,4 +1,4 @@
-import assert from 'node:assert';
+import assert from 'assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
 import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';

src/pages/popup/components/text.tsx
@@ -1,4 +1,4 @@
-import assert from 'node:assert';
+import assert from 'assert';
 import React, { ReactNode } from 'react';
 import { NoChildren, NonStoredProps, StoredVirtualNodeProps } from '../base';
 import { BaseUpdateMsg, validateBaseUpdateMsg } from '../msgs';

src/pages/popup/msgs.ts
@@ -8,7 +8,7 @@ export {
     UpdateProcessingInstructionMsg,
     UpdateTextMsg
 } from './components';
-import assert from 'node:assert';
+import assert from 'assert';
 import {
     UpdateAttributeMsg,
     UpdateCdataSectionMsg,

webpack/webpack.config.ts
@@ -419,6 +419,12 @@ const config: webpack.Configuration = {
             });
         }),
 
+        // assert polyfill depends on process
+        // https://github.com/browserify/commonjs-assert/issues/55#issuecomment-996543717
+        new webpack.ProvidePlugin({
+            process: 'process/browser'
+        }),
+
         // Packaging popup entry point
         new HtmlWebpackPlugin({
             template: path.join(