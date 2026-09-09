0098cae1289a57604ea10fba8b915b7bb64efb3e
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-09-09 05:41:45 -0400

    chore: Begin to migrate from `html-webpack-plugin` to native Webpack HTML bundling
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- webpack/config/html.ts
+++ webpack/config/html.ts
@@ -1,49 +1,10 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'node:path';
 import { LICENSE } from '../assets/license';
-import { PROJECT_ROOT } from '../env';
 import {
     CreateHtmlSourceMapWebpackPlugin,
     HtmlBannerWebpackPlugin
 } from '../plugins';
 
-export const moduleRules = [
-    {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: 'html-loader'
-    }
-];
-
 export const plugins = [
-    // Packaging popup entry point
-    new HtmlWebpackPlugin({
-        template: path.join(
-            PROJECT_ROOT,
-            'src',
-            'pages',
-            'popup',
-            'index.html'
-        ),
-        filename: path.join('popup', 'index.html'),
-        chunks: ['popup'],
-        minify: 'auto'
-    }),
-
-    // Packaging options entry point
-    new HtmlWebpackPlugin({
-        template: path.join(
-            PROJECT_ROOT,
-            'src',
-            'pages',
-            'options',
-            'index.html'
-        ),
-        filename: path.join('options', 'index.html'),
-        chunks: ['options'],
-        minify: 'auto'
-    }),
-
     // Manually creating (unlinked) source map
     new CreateHtmlSourceMapWebpackPlugin(),
 
