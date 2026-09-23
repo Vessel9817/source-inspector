4e21b05118fa4631dabdba5fb538424a9ab689a1
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-09-23 16:22:00 -0400

    chore!: Remove unused plugins
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- webpack/plugins/index.ts
+++ webpack/plugins/index.ts
@@ -1,3 +1,1 @@
-export { default as HtmlBannerWebpackPlugin } from './htmlBanner';
-export { default as CreateHtmlSourceMapWebpackPlugin } from './htmlSourceMap';
 export { default as GenerateFilePlugin } from './genFile';
