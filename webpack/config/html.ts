509ff85c81a2cd998e0c7231f8b15acbdb20ffc6
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-09-23 17:11:50 -0400

    fix: Remove dead code
    Patches 4e21b05118fa4631dabdba5fb538424a9ab689a1
    
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- webpack/config/html.ts
+++ webpack/config/html.ts
@@ -1,9 +1,4 @@
 import type webpack from 'webpack';
-import { LICENSE } from '../assets/license';
-import {
-    CreateHtmlSourceMapWebpackPlugin,
-    HtmlBannerWebpackPlugin
-} from '../plugins';
 import { filenameTemplate } from './output';
 
 /**
@@ -23,14 +18,3 @@ export const output: webpack.Configuration['output'] = {
     htmlFilename: template,
     htmlChunkFilename: template
 };
-
-export const plugins: webpack.Configuration['plugins'] = [
-    // Manually creating (unlinked) source map
-    new CreateHtmlSourceMapWebpackPlugin(),
-
-    // Embedding license information after minimization
-    new HtmlBannerWebpackPlugin({
-        banner: LICENSE,
-        sourceMap: true
-    }),
-];
